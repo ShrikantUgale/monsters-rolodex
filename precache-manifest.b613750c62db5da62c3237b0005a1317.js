d58b8f3be1faf57390072cc017048d7d3bee4f73
Shrikant <itzshrikant@gmail.com>
2020-03-11 12:17:28 +0530

    Updates

---
+++ precache-manifest.b613750c62db5da62c3237b0005a1317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b2f04c9f23f3c56d81b35c4b0a62df",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "03e24194275b69144417",
+    "url": "/monsters-rolodex/static/css/main.75f1fd43.chunk.css"
+  },
+  {
+    "revision": "fdebec41ff4777bb5759",
+    "url": "/monsters-rolodex/static/js/2.7827a466.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.7827a466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e24194275b69144417",
+    "url": "/monsters-rolodex/static/js/main.58626fce.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
